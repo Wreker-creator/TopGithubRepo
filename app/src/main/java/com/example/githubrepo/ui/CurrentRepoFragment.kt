615c0d0afa49194f58f9e5aac33d31b661883fd8
Wreker-creator <ananyladha28@gmail.com>
2022-05-06 18:08:41 +0530

    Second Fragment is ready now with basic details about the repo

--- app/src/main/java/com/example/githubrepo/ui/CurrentRepoFragment.kt
+++ app/src/main/java/com/example/githubrepo/ui/CurrentRepoFragment.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Adapter
 import androidx.navigation.fragment.navArgs
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.drawable.DrawableTransitionOptions
 import com.example.githubrepo.R
 import com.example.githubrepo.databinding.FragmentCurrentRepoBinding
 import com.example.githubrepo.viewModel.GitHubViewModel
@@ -26,7 +28,16 @@ class CurrentRepoFragment : Fragment() {
         currentRepoBinding = FragmentCurrentRepoBinding.inflate(inflater, container, false)
         val view = currentRepoBinding.root
 
-        currentRepoBinding.CurrentRepoName.text = args.repo.full_name
+        currentRepoBinding.CurrentRepoName.text = args.repo.name
+        Glide.with(view).load(args.repo.owner?.avatar_url).transition(DrawableTransitionOptions.withCrossFade()).into(currentRepoBinding.CurrentRepoImage)
+        currentRepoBinding.CurrentRepoStarCount.text = args.repo.stargazers_count.toString()
+
+        if(args.repo.description.toString().isNotEmpty()){
+            currentRepoBinding.CurrentRepoDescription.text = args.repo.description.toString()
+        }else{
+            currentRepoBinding.CurrentRepoDescription.visibility = View.GONE
+        }
+
 
         return view
     }
